b273e2c3615909635b87a8250bc95652be21db10
chukwuebukah33 <uchegbulemhamilton@gmail.com>
2025-06-28 10:04:19 +0100

    company management project
    company management project

---
+++ employees/views.py
@@ -0,0 +1,22 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Manager, Intern
+from .serializers import ManagerSerializer, InternSerializer
+
+class StaffRoleView(APIView):
+    def get(self, request):
+        roles = []
+
+        for manager in Manager.objects.all():
+            roles.append({
+                "name": f"{manager.first_name} {manager.last_name}",
+                "role": manager.get_role()
+            })
+
+        for intern in Intern.objects.all():
+            roles.append({
+                "name": f"{intern.first_name} {intern.last_name}",
+                "role": intern.get_role()
+            })
+
+        return Response(roles)
